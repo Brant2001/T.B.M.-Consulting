73c72c1a3e1c74cb9db338e9cbbf0235263304a5
Brant2001 <brant.pippin@gmail.com>
2020-04-07 15:00:57 -0500

    added a last name to the employees

--- scripts/employees/Employee.js
+++ scripts/employees/Employee.js
@@ -2,7 +2,7 @@ export const Employee = (employeeObject, computer) => {
     return `
         <div class="employee">
             <header>
-                <h3>${employeeObject.firstName}</h3>
+                <h3>${employeeObject.firstName} ${employeeObject.lastName}</h3>
             </header>
             <p>Age: ${employeeObject.age}</p>
             <p>Computer: ${computer.model}</p>
